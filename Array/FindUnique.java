623df54a46d09b3f649736fe1f1e2d64b0314dbd
Kingsuk Dhar <kingsuk@eazydiner.com>
2022-11-03 15:08:28 +0530

    added binary search related problems

---
+++ Array/FindUnique.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class FindUnique{
+  public static int findUnique(int arr[]){
+
+    for(int i=0; i<arr.length; i++){
+      
+    }
+  }
+}
---
+++ Array/link.js
@@ -0,0 +1,35 @@
+const link = {
+  'find_unique_element': 'https://www.codingninjas.com/codestudio/problems/find-unique_625159?source=youtube&campaign=love_babbar_codestudio1&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio1',
+
+  'unique_no_of_occurrences': 'https://leetcode.com/problems/unique-number-of-occurrences/',
+
+  'find_duplicates_in_an_array': 'https://www.codingninjas.com/codestudio/problems/duplicate-in-array_893397?source=youtube&campaign=love_babbar_codestudio1&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio1',
+
+  'find_all_duplicates_in_an_array': 'https://leetcode.com/problems/find-all-duplicates-in-an-array/',
+
+  'find_intersection_in_array': 'https://www.codingninjas.com/codestudio/problems/intersection-of-2-arrays_1082149?source=youtube&campaign=love_babbar_codestudio1&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio1',
+
+  'pair_sum': 'https://www.codingninjas.com/codestudio/problems/pair-sum_697295?source=youtube&campaign=love_babbar_codestudio1&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio1',
+
+  'sort_0_1': 'https://www.codingninjas.com/codestudio/problems/sort-0-1_624379',
+
+  // Binary Search
+
+  'first_and_last_occurences': 'https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/',
+
+  'peak_in_a_mountain': 'https://leetcode.com/problems/peak-index-in-a-mountain-array/description/',
+
+  'find_pivot_index': 'https://leetcode.com/problems/find-pivot-index/',
+
+  'search_in_sorted_rotated_array': 'https://www.codingninjas.com/codestudio/problems/search-in-rotated-sorted-array_1082554?leftPanelTab=0',
+
+  // advance binary search
+
+  'book_allocation_problem': 'https://www.codingninjas.com/codestudio/problems/allocate-books_1090540',
+
+  '' : '',
+  
+
+
+};
+
